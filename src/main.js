c2f800d7c19623bb55614b423efd93c6921e081f
lauren <laurenweng@cw.com.tw>
2025-01-24 12:43:42 +0800

    安裝tailwind sass fontAwesomeIcon

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
+// 管理 Vue 的應用程式層級、Vue 特定功能或元件
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+// tailwind
+import './assets/tailwind.css'
+
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+library.add(fas, far, fab)
 
+const app = createApp(App)
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
-
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': '/src',
     },
   },
 })
