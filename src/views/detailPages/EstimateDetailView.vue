ab6e424b883e7a4794e8f529b27b2b503e48fc21
lauren <laurenweng@cw.com.tw>
2025-02-01 00:27:48 +0800

    建立pinia store

--- src/views/detailPages/EstimateDetailView.vue
+++ src/views/detailPages/EstimateDetailView.vue
@@ -1,5 +1,24 @@
+<template>
+  <div class="container">
+    <Title>估時紀錄 | {{ props.tableData.backlogNum }}</Title>
+    <h1>詳細頁</h1>
+    <p>當前 ID: {{ route.params.id }}</p>
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Title from '@/components/Title.vue'
+import { useRoute } from 'vue-router'
+import { computed, onUnmounted } from 'vue'
+const route = useRoute()
+const paramsId = computed(() => route.params.id)
+
+// const props = defineProps({
+//   tableData: {
+//     type: Array,
+//     required: true,
+//   },
+// })
 </script>
 
 <template>
