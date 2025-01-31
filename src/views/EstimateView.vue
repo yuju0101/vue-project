c6891a98ecb892dc0be275001f9c44e23eaaee33
lauren <laurenweng@cw.com.tw>
2025-02-01 00:34:44 +0800

    串接內部api

--- src/views/EstimateView.vue
+++ src/views/EstimateView.vue
@@ -53,46 +53,66 @@ watch(paramsId, (newId) => {
 
 const tableHeaders = [
   {
-    id: '1',
-    bcaklogNum: '97453',
-    project: 'Chatbot客服機器人',
-    name: '前台客服傳連結訊息可直接點擊',
-    estimateTime: '2',
-    reactiveTime: '2',
-    status: '已完成',
-    link: 'https://dev.azure.com/cwgroup/%E7%87%9F%E9%81%8B%E7%B3%BB%E7%B5%B1%E7%B5%84/_boards/board/t/Data%20Middle/Backlog%20items?System.AssignedTo=laurenweng%40cw.com.tw&workitem=97453',
+    key: 'id',
+    width: '5%',
+    label: '#',
+    slotName: 'id',
   },
   {
-    id: '2',
-    bcaklogNum: '97464',
-    project: 'Chatbot客服機器人',
-    name: '滿意度調查後台管理頁面抽屜呈現',
-    estimateTime: '8',
-    reactiveTime: '16',
-    status: '暫停中',
-    link: 'https://dev.azure.com/cwgroup/%E7%87%9F%E9%81%8B%E7%B3%BB%E7%B5%B1%E7%B5%84/_boards/board/t/Data%20Middle/Backlog%20items?System.AssignedTo=laurenweng%40cw.com.tw&workitem=97464',
+    key: 'backlogNum',
+    width: '10%',
+    label: 'Backlog',
   },
   {
-    id: '3',
-    bcaklogNum: '97453',
-    project: 'TIT數位專輯',
-    name: '列表與detail頁串接/資料處理',
-    estimateTime: '21',
-    reactiveTime: '16',
-    status: '進行中',
-    link: 'https://dev.azure.com/cwgroup/digital-products/_boards/board/t/digital-products%20Team/Backlog%20items?System.AssignedTo=kelvinchen%40cw.com.tw%2Claurenweng%40cw.com.tw&workitem=97924',
+    key: 'project',
+    width: '10%',
+    label: '專案名稱',
   },
   {
-    id: '4',
-    bcaklogNum: '97707',
-    project: 'cs-admin合併至it-admin',
-    name: '會員列表/會員資料/自動續訂/訂戶權益',
-    estimateTime: '30',
-    reactiveTime: '36',
-    status: '未完成',
-    link: 'https://dev.azure.com/cwgroup/%E7%87%9F%E9%81%8B%E7%B3%BB%E7%B5%B1%E7%B5%84/_workitems/edit/97707',
+    key: 'name',
+    width: '10%',
+    label: '專案內容',
+  },
+  {
+    key: 'estimateTime',
+    width: '10%',
+    label: '預估時數',
+    slotName: 'estimateTime',
+  },
+  {
+    key: 'reactiveTime',
+    width: '10%',
+    label: '實際完成時數',
+    slotName: 'reactiveTime',
+  },
+  {
+    key: 'status',
+    width: '10%',
+    label: '狀態',
+    // 在想要監控的地方必須定義 slotName
+    slotName: 'status',
+  },
+  {
+    key: 'link',
+    width: '10%',
+    label: '連結',
+    slotName: 'link',
   },
 ]
+
+// api 串接
+const tableData = ref([])
+const apiUrl = 'http://localhost:3000'
+const fetchTableData = async () => {
+  try {
+    const fetchDataUrl = `${apiUrl}/estimates`
+    const response = await fetch(fetchDataUrl)
+    const result = await response.json()
+    tableData.value = result.data
+  } catch (error) {
+    console.error('Fetch API failed:', error)
+  }
+}
 
 const statusCheck = (status) => {
   if (status) {
